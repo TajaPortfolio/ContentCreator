5a19206bddb6097bc013f58d96ac3be7e5e78559
tajahouse <ahlliahtaja@yahoo.com>
2022-12-16 11:34:19 +0100

    Code Clean up

--- components/Footer.js
+++ components/Footer.js
@@ -2,13 +2,13 @@ const Footer = () => {
     return (
         <>
             <footer class="footer">
-                    <div class="content has-text-centered">
+                <div class="content has-text-centered">
                     <p>
                         <strong>Bulma - Blog theme</strong> by <a href="https://gonzalojs.com">Gonzalo Gutierrez</a>. Based on the <a
                         href="http://jigsaw-blog-staging.tighten.co/">jigsaw-blog</a>. The source code is licensed
                         <a href="http://opensource.org/licenses/mit-license.php">MIT</a>
                     </p>
-                    </div>
+                </div>
             </footer>
         </>
     )
