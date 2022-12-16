738da546ed5f3d623e8df887465ac22d0c30a182
tajahouse <ahlliahtaja@yahoo.com>
2022-12-16 10:56:16 +0100

    Initializing Project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '../styles/globals.css'
+import "bulma/css/bulma.min.css";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MainApp = ({Component, pageProps}) =>{
+    return(
+        <Component {...pageProps} />
+    )
 }
+export default MainApp;
